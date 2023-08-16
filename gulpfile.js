cd6be1458c9356b101157c951694508a6748e8f0
Richard <richardsanvie@gmail.com>
2023-08-16 09:52:43 -0300

    Setup of project

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass(){
+    return gulp.src('./source/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./build/'));
+}
+
+exports.default = function(){
+    gulp. watch('.source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+}
